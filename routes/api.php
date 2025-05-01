881cd971a17613e49dddd59beb97d597fc14d4e2
Sachin Karki <itssachin079@gmail.com>
2025-05-01 10:25:53 +0545

    Changed route name

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\UserController;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register API routes for your application. Thesesdasdsasadasdasd
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -27,7 +27,6 @@ Route::get('/settings',[homeController::class,'settings']);
 Route::get('/event_details/{id}',[homeController::class,'event_details']); //single event details page
 Route::get('/events/{id}',[homeController::class,'events']); //event category show all events
 // Route::get('/all_events',[homeController::class,'all_events']); //all events page
-Route::post('/contact',[homeController::class,'contactStore']); //contact page form store
 Route::get('/booking/{id}',[UserController::class,'booking_details']);
 Route::post('/booking/{id}',[UserController::class,'event_booking']);
 Route::get('/all_events',[homeController::class,'search_event']);  
@@ -39,10 +38,7 @@ Route::post('/resetpassword',[UserController::class,'resetpasswordUpdate']);
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/user',[UserController::class,'getUser']); //get user by token
     Route::get('/my_events',[UserController::class,'my_events']);  //All user events by user token
-    Route::post('/logout',[UserController::class,'logout']);  //user logout by token
-    Route::post('/changepassword',[UserController::class,'changepassword']);  //user password change by token
     Route::get('/view_event/{id}',[UserController::class,'view_event']); //booking table view event detail
-    Route::post('/myprofile',[UserController::class,'myprofile']); //booking table view event detail
 });
 
 
